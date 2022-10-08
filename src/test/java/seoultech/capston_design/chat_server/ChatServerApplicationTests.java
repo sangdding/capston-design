6aa6bc63878738018e57b3df32303dce5f62549c
sangmyung <enjxz97382@gmail.com>
2022-10-08 16:26:59 +0900

    project init

---
+++ src/test/java/seoultech/capston_design/chat_server/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package seoultech.capston_design.chat_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
