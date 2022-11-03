4671499c7e2fbd31b8a6d2b226cae328cc41ca9d
sangmyung <enjxz97382@gmail.com>
2022-11-03 14:12:16 +0900

    Feat: 로그인 요청 컨트롤러 생성
    현재 리턴 값은 임시로 설정함. 수정 필요.
    
    Related to: #1

---
+++ src/main/java/agora_main_server/controller/UserLoginController.java
@@ -0,0 +1,34 @@
+package agora_main_server.controller;
+
+import agora_main_server.dto.UserFormDto;
+import agora_main_server.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(value = "/login")
+@Log4j2
+public class UserLoginController {
+
+    private final UserService userService;
+
+    // 로그인
+    @PostMapping(value = "/")
+    public String login(@RequestParam String userId, String password) {
+       log.info("#login page");
+       return "/main";
+    }
+
+    // 회원가입
+    @PostMapping(value = "/register")
+    public String register(@RequestParam UserFormDto userFormDto) {
+        log.info("#register page");
+        return "/login";
+    }
+
+}
