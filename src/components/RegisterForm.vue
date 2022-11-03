2ee965e79d10f2b01f6bef08b8e68bb475b0f160
sangmyung <enjxz97382@gmail.com>
2022-11-03 14:13:15 +0900

    Feat: 로그인 폼 생성
    로그인 폼 생성. 불필요한 파일 제거 필요
    
    Related to: #1

---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="RegisterForm">
+      <div>
+        <label for="name"></label>
+      </div>
+    </form>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    RegisterForm: function() {
+      this.axios.post("")
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
