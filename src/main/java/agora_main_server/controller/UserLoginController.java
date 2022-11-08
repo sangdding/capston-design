3e42cd62e9c74d658ec976c80b1891108216ee75
sangmyung <enjxz97382@gmail.com>
2022-11-08 19:52:34 +0900

    Chore: Cors 설정 구현
    불필요한 filter 및 config 제거
    
    Related to: #1

--- src/main/java/agora_main_server/controller/UserLoginController.java
+++ src/main/java/agora_main_server/controller/UserLoginController.java
@@ -1,36 +1,38 @@
 package agora_main_server.controller;
 
+import agora_main_server.dto.ResultDto;
 import agora_main_server.dto.UserFormDto;
-import agora_main_server.entity.User;
 import agora_main_server.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping(value = "/login")
+@RequestMapping("/")
 @Log4j2
 public class UserLoginController {
 
     private final UserService userService;
 
     // 로그인
-    @PostMapping(value = "/")
-    public String login(@RequestParam String userId, String password) {
+    @PostMapping(value = "/login")
+    public ResultDto<Object> login(@RequestParam String userId, String password) {
        log.info("#login page");
-       return "/main";
+       return ResultDto.ofSuccess();
     }
 
     // 회원가입
     @PostMapping(value = "/register")
-    public String register(@RequestParam UserFormDto userFormDto) {
+    @ResponseBody
+    public ResultDto<Object> register(@RequestBody UserFormDto userFormDto) {
         log.info("#register page");
-        userService.saveUser(userService.createUser(userFormDto));
-        return "/login";
+        try {
+            userService.saveUser(userService.createUser(userFormDto));
+            return ResultDto.ofSuccess();
+        } catch (Exception e) {
+            return ResultDto.ofFail(e.getMessage());
+        }
     }
-
 }
