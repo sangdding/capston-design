e4ffa3b21611d776f9a6bf285eead2461984e17b
sangmyung <enjxz97382@gmail.com>
2023-01-11 18:07:54 +0900

    init

--- main-server/src/main/java/agora_main_server/entity/UserRole.java
+++
@@ -1,5 +0,0 @@
-package agora_main_server.entity;
-
-public enum UserRole {
-    USER, ADMIN;
-}
--- main-server/src/main/java/agora_main_server/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package agora_main_server.repository;
-
-import agora_main_server.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
