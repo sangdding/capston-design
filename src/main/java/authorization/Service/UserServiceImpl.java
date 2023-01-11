2ba27fcd33a8b35b9097b5542fbea68d1538bd0a
sangmyung <enjxz97382@gmail.com>
2023-01-11 16:43:41 +0900

    init

---
+++ src/main/java/authorization/Service/UserServiceImpl.java
@@ -0,0 +1,5 @@
+package authorization.Service;
+
+public class UserServiceImpl implements UserSer{
+
+}
---
+++ src/main/java/authorization/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package authorization.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailService).passwordEncoder()
+    }
+}
